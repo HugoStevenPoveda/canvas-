4659aaaa21e7eed15edc902ae958407f934cf32f
Your Name <hugogirata91@hotmai.com>
2021-04-26 21:15:21 -0500

    canvas

--- dibujo.js
+++ dibujo.js
@@ -1,7 +1,33 @@
 
+var texto = document.getElementById("cash");
+var buscar= document.getElementById("boton");
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var n =0 ;
+
+buscar.addEventListener("click",contar);
+var a = d.width;
+console.log(a);
+  //alert(a);
+
+
+function contar()
+ {
+        var x =parseInt( texto.value) ;
+        console.log(x);
+       var numeroLi =(a/x);
+       var numeriLi2=(a/x);
+
+       for(var i=0 ; i<= a ;i=i+1 )
+             {
+               Dibulinea("#76B799",0,i*numeroLi,i*numeroLi,a);
+
+               Dibulinea("#D4562A ",0,a-i*numeroLi,i*numeroLi,0);
+
+               Dibulinea("#798DCB",a,i*numeroLi,a-i*numeroLi,a);
+               Dibulinea("#8A222A ",a-i*numeroLi,0,a,a-i*numeroLi);
+             }
+    }
+
 
 
 function Dibulinea(color,x_inicial, y_inicial, x_final, y_final)
@@ -13,26 +39,3 @@ function Dibulinea(color,x_inicial, y_inicial, x_final, y_final)
   lienzo.stroke();
   lienzo.closePath();
 }
-
-for(var i=0 ; i<=300;i++ )
-{
-    Dibulinea("#76B799",0,i*10,i*10,300);
-    Dibulinea("#D4562A ",0,300-i*10,i*10,0);
-    Dibulinea("#798DCB",300,i*10,300-i*10,300);
-    Dibulinea("#8A222A ",300-i*10,0,300,300-i*10);
-}
-
-while(n<=14)
-{
-    n++;
-    Dibulinea("#76B799",150, n*10,150- n*10,150);
-    Dibulinea("#798DCB",150,n*10,150+n*10,150);
-    Dibulinea("#8A222A ",150,300- n*10,150- n*10,150);
-    Dibulinea("#D4562A ",150,300-n*10,150+n*10,150);
-}
-    Dibulinea("#76B799",299,1,299,299);
-    Dibulinea("#D4562A ",1,1,299,1);
-    Dibulinea("#76B799",1,299,299,299);
-    Dibulinea("#2B0104  ",1,1,1,299);
-    Dibulinea("black",150,1,150,299);
-    Dibulinea("black ",1,150,299,150);
